dfb9760e4912f6d60e8094b33bf4cc4ff9cb7f3f
Rare <group.scala@mail.ru>
2023-12-02 18:48:41 +0500

    test

--- config/params.php
+++ config/params.php
@@ -4,6 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'bot_api_key' => '6958846224:AAHybx3mg55THx9ZbGTp0Gz_rUJDd7nJGv8',
+    'bot_api_key' => '',
     'username_bot' => 'gm_saleping_bot',
 ];
